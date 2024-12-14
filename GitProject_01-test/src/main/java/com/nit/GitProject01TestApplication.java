1a5b404f0f9657aa839f358aa17e16512640ba2d
Dibyaranjan Behera <166224882+Dibyaranjan-Behera@users.noreply.github.com>
2024-12-15 00:52:54 +0530

    initial Commit - TL

---
+++ GitProject_01-test/src/main/java/com/nit/GitProject01TestApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitProject01TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProject01TestApplication.class, args);
+	}
+
+}
